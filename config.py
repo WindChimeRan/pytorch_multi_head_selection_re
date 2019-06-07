df6d8c703075d1c28ef76104ab7a60fb5fc636ba
WindChimeRan <1012869439@qq.com>
2019-06-07 15:52:58 +0800

    vocab?

---
+++ config.py
@@ -0,0 +1,18 @@
+class Hyper(object):
+    def __init__(self):
+        pass
+
+class Config(object):
+    def __init__(self):
+        pass
+
+    def preprocessing(self):
+        pass
+
+    def run(self):
+        pass
+
+
+if __name__ == "__main__":
+    config = Config()
+    config.run()
